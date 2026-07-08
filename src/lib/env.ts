36d35cc0504ff2f22efe5e6eb34d783001720812
BadLuckZ <thanagorn8802@gmail.com>
2026-07-08 20:38:37 +0700

    feat: redirect content

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,17 @@
-export const appConfig = {
+type AppConfigType = {
+  appName: string;
+  apiBaseUrl: string;
+  redirectAt: number;
+  redirectTo: string;
+};
+
+export const appConfig: AppConfigType = {
   appName: import.meta.env.PUBLIC_APP_NAME || "firstdate 2026",
   apiBaseUrl: import.meta.env.PUBLIC_API_BASE_URL || "http://localhost:3000",
 
   // go to /redirect when 18 Jun 19:00
   redirectAt: new Date("2026-07-18T19:00:00+07:00").getTime(),
+
+  // TODO: go to rpkm's path
+  redirectTo: "/",
 } as const;
